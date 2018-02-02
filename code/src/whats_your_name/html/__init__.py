5f29531847e8a029ab2b129f6b27a905bf22bc0e
Piotr Mlocek <pimlock@gmail.com>
2018-02-01 21:43:18 -0800

    Update the UI.

--- code/src/whats_your_name/html/__init__.py
+++ code/src/whats_your_name/html/__init__.py
@@ -1,70 +1,109 @@
 file_upload_page = '''
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <img id="preview" src=""/>
-        <form>
-          <input id="file" type="file">
-          <input id="go" type="button" value="Recognize!" style="display: none">
-        </form>
-        <div id="result"></div>
-        
-        <script type="text/javascript">
-            $(function () {
-                var goButton = $('#go');
-                var preview = $('#preview');
-                var fileInput = $('#file');
-                var result = $('#result');
-            
-                var getFile = function () {
-                    return fileInput.get(0).files[0];
-                }
-            
-                fileInput.bind('change', function () {
-                    var reader = new FileReader();
-                    var file = getFile();
-            
-                    reader.addEventListener("load", function () {
-                        preview.attr('src', reader.result);
-                        goButton.show();
-                    }, false);
-            
-                    if (file) {
-                        reader.readAsDataURL(file);
-                    }
-                });
-            
-                goButton.bind('click', function () {
-                    var reader = new FileReader();
-                    var file = getFile();
-            
-                    reader.addEventListener("load", function () {
-                        preview.attr('src', reader.result);
-                            $.ajax({
-                                url: '<%APP_URL%>',
-                                type: 'POST',
-                                data: reader.result,
-                                success: function (data) {
-                                    if (data['faces'] && data['faces'].length > 0) {
-                                        result.html("Found a match! It's <strong>" + data['faces'].join(', ') + "</strong>")
-                                    } else {
-                                        result.text("I didn't find a match :/")
-                                    }
-                                },
-                                dataType: 'json',
-                                contentType: 'application/json'
-                            })
-                    }, false);
-            
-                    if (file) {
-                        reader.readAsDataURL(file);
-                    }
-                });
-            });
-        </script>
-    </body>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+    <title>What's Your Name?</title>
+
+    <style type="text/css">
+    </style>
+</head>
+<body>
+<div class="container mt-2">
+    <div class="row justify-content-md-center">
+        <div class="col-md-10 col-lg-8">
+            <div class="jumbotron">
+                <h1 class="display-4 text-center">What's Your Name?!</h1>
+                <p class="lead text-center">Take a photo and find out what's their name!</p>
+                <div class="row">
+                    <div class="col py-2">
+                        <div class="media d-none" id="preview">
+                            <img class="mr-3 rounded img-responsive w-50" src="" alt="Uploaded photo">
+                            <div class="media-body">
+                                <div id="result">
+                                    <span class="loading">Checking... <i class="fas fa-circle-notch fa-spin"></i></span>
+                                    <span class="name"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col">
+                        <form>
+                            <div class="input-group mb-3">
+                                <div class="custom-file">
+                                    <input type="file" class="btn-primary custom-file-input" id="file">
+                                    <label class="custom-file-label" for="file">Select a photo</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+<script type="text/javascript">
+    $(function () {
+        var preview = $('#preview');
+        var fileInput = $('#file');
+        var result = $('#result');
+
+        var getFile = function () {
+            return fileInput.get(0).files[0];
+        };
+
+        fileInput.bind('change', function () {
+            var reader = new FileReader();
+            var file = getFile();
+
+            reader.addEventListener("load", function () {
+                preview.find('img').attr('src', reader.result);
+                preview.removeClass('d-none');
+                result.find('.loading').show();
+
+                $.ajax({
+                    type: 'POST',
+                    data: reader.result,
+                    success: function (data) {
+                        result.find('.loading').hide();
+
+                        var name = result.find('.name');
+                        if (data['faces'] && data['faces'].length > 0) {
+                            name.html("Found a match! It's <strong>" + data['faces'].join(', ') + "</strong>")
+                        } else {
+                            name.text("I didn't find a match :/")
+                        }
+                    },
+                    error: function() {
+                        result.find('.loading').hide();
+                        result.find('.name').text('Something went wrong :/')
+                    },
+                    dataType: 'json',
+                    contentType: 'application/json'
+                })
+            }, false);
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        });
+    });
+</script>
 </html>
 '''
